0cbc599dd5be90e4a1183a4580a41b110e4d44a5
Sebastian <hawaiilive@outlook.com>
2026-05-07 23:54:01 +0200

    with custom pieces and lengths

---
+++ Assets/Scripts/World/Rail/Track/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public struct TrackInfo
+{
+    public bool ShouldTendUp;
+    public bool ShouldTendRight;
+    public bool HasReachedUp;
+    public bool HasReachedRight;
+}
+public class Segment
+{
+    public Vector2Int direction;
+    public float distancePercent;
+    public float distancePercentVariation;
+}
+public abstract class Piece
+{
+    public List<Segment> segments;
+    public abstract int Weight(Vector2Int current, Vector2Int end, TrackInfo info);
+}
---
+++ Assets/Scripts/World/Rail/Track/Pieces/PieceRight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+class PieceRight : Piece
+{
+    public PieceRight()
+    {
+        segments = new()
+        {
+            new Segment
+            {
+                direction = Vector2Int.right,
+                distancePercent = 0.05f,
+                distancePercentVariation = 0.02f
+            }
+        };
+    }
+
+    public override int Weight(Vector2Int current, Vector2Int end, TrackInfo info)
+    {
+        if (info.ShouldTendRight)
+        {
+            return 8;
+        }
+
+        return 1;
+    }
+}
