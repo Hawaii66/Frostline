7343b3ead7e0f2098e8dd8e7fb0c92e2110bba61
Sebastian <hawaiilive@outlook.com>
2026-05-12 04:35:00 +0200

    attempt at render around players

---
+++ Assets/Scripts/Render/HeightRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Frostline.Renderer
+{
+    public class HeightRenderer : MonoBehaviour, IRendererHandler
+    {
+        private ObjectPool<IPoolable> pool;
+
+        private void Start()
+        {
+            pool = new(5, () => null);
+            RenderereViewerManager.Instance.AddListener(this);
+        }
+        private void OnDestroy()
+        {
+            RenderereViewerManager.Instance.RemoveListener(this);
+        }
+
+        public void ChangeVisible(Vector2Int[] inRange)
+        {
+            Debug.Log("Change visible" + inRange.Length);
+        }
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿namespace Frostline.Core
+{
+    public abstract class Singleton<T> where T : Singleton<T>, new()
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new();
+                }
+                return _instance;
+            }
+        }
+    }
+}
